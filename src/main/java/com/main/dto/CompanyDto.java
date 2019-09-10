f60ce12f5c1ecd9850ddc3d5fb1174fae47b8d09
dominik <dominwro@gmail.com>
2019-09-10 20:33:41 +0200

    Add CompanyDto class.

---
+++ src/main/java/com/main/dto/CompanyDto.java
@@ -0,0 +1,158 @@
+package com.main.dto;
+
+public class CompanyDto
+{
+    private Long id;
+    private String name;
+    private String type;
+    private String nio;
+    private String regon;
+    private String krs;
+    private String region;
+    private String city;
+    private String street;
+    private String number;
+    private String postcode;
+    private String contactPersonName;
+    private String contactPersonSurname;
+    private String phoneNumber;
+    private String contactPersonName2;
+    private String contactPersonSurname2;
+    private String phoneNumber2;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getNio() {
+        return nio;
+    }
+
+    public void setNio(String nio) {
+        this.nio = nio;
+    }
+
+    public String getRegon() {
+        return regon;
+    }
+
+    public void setRegon(String regon) {
+        this.regon = regon;
+    }
+
+    public String getKrs() {
+        return krs;
+    }
+
+    public void setKrs(String krs) {
+        this.krs = krs;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getContactPersonName() {
+        return contactPersonName;
+    }
+
+    public void setContactPersonName(String contactPersonName) {
+        this.contactPersonName = contactPersonName;
+    }
+
+    public String getContactPersonSurname() {
+        return contactPersonSurname;
+    }
+
+    public void setContactPersonSurname(String contactPersonSurname) {
+        this.contactPersonSurname = contactPersonSurname;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getContactPersonName2() {
+        return contactPersonName2;
+    }
+
+    public void setContactPersonName2(String contactPersonName2) {
+        this.contactPersonName2 = contactPersonName2;
+    }
+
+    public String getContactPersonSurname2() {
+        return contactPersonSurname2;
+    }
+
+    public void setContactPersonSurname2(String contactPersonSurname2) {
+        this.contactPersonSurname2 = contactPersonSurname2;
+    }
+
+    public String getPhoneNumber2() {
+        return phoneNumber2;
+    }
+
+    public void setPhoneNumber2(String phoneNumber2) {
+        this.phoneNumber2 = phoneNumber2;
+    }
+}
