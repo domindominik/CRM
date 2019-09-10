327577aaf1a6ae0f455917711f6a6f8dffd0630c
dominik <dominwro@gmail.com>
2019-09-10 19:54:52 +0200

    Add TypeModel class.

---
+++ src/main/java/com/main/model/TypeModel.java
@@ -0,0 +1,31 @@
+package com.main.model;
+
+import javax.persistence.*;
+
+@Entity
+public class TypeModel
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column
+    private int id;
+
+    @Column
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
