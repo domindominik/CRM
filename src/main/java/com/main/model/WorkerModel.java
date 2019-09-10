eaf9867cc6bced47799a8e44a29f4d515d627000
dominik <dominwro@gmail.com>
2019-09-10 19:47:25 +0200

    Add WorkerModel class.

---
+++ src/main/java/com/main/model/WorkerModel.java
@@ -0,0 +1,78 @@
+package com.main.model;
+
+import org.hibernate.validator.constraints.UniqueElements;
+
+import javax.persistence.*;
+
+@Entity
+public class WorkerModel
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private  String surname;
+
+    @UniqueElements
+    @Column
+    private String email;
+
+    @Column
+    private String password;
+
+    @Column
+    private int type;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+}
