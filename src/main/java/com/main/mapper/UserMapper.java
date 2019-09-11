f4db372f12a5a49237a53e9aaa2bc9adf080c144
dominik <dominwro@gmail.com>
2019-09-11 20:19:38 +0200

    Add UserMapper class.

---
+++ src/main/java/com/main/mapper/UserMapper.java
@@ -0,0 +1,29 @@
+package com.main.mapper;
+
+import com.main.dto.UserDto;
+import com.main.model.UserModel;
+
+public class UserMapper
+{
+    public static UserDto toDto(UserModel userModel)
+    {
+        UserDto userDto = new UserDto();
+        userDto.setName(userModel.getName());
+        userDto.setSurname(userModel.getSurname());
+        userDto.setEmail(userModel.getEmail());
+        userDto.setPassword(userModel.getPassword());
+        userDto.setType(userModel.getType());
+        return userDto;
+    }
+
+    public static UserModel toEntity(UserDto userDto)
+    {
+        UserModel userModel = new UserModel();
+        userModel.setName(userDto.getName());
+        userModel.setSurname(userDto.getSurname());
+        userModel.setEmail(userDto.getEmail());
+        userModel.setPassword(userDto.getPassword());
+        userModel.setType(userDto.getType());
+        return userModel;
+    }
+}
